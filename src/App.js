ac78d494cd260936293672daeb0956a87f80eda6
Randattt <rvlad2109@mail.ru>
2021-11-22 19:24:39 +0300

    feat(Phone input mask): added mask with basic methods, v.1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import PhoneMask from "./components/phone-mask/PhoneMask";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <PhoneMask/>
+  )
 }
-
 export default App;
